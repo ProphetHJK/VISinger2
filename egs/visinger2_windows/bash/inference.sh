00e05fdf1bda1cd53a33a00ecb3555d393607bc4
prophethjk <prophethjk@gmail.com>
2023-05-09 15:13:11 +0800

    修复torch2.0的istft不支持简单tensor的问题 修改requirements.txt 修改日志打印等级 修改readme.md格式

--- egs/visinger2_windows/bash/inference.sh
+++ egs/visinger2_windows/bash/inference.sh
@@ -5,8 +5,8 @@ base_dir=$(dirname $(dirname $exp_dir))
 export PYTHONPATH=$base_dir
 export PYTHONIOENCODING=UTF-8
 
-CUDA_VISIBLE_DEVICES=0 python inference.py \
-    -model_dir /root/logdir/visinger2 \
-    -input_dir data/opencpop/testset.txt \
-    -output_dir /root/logdir/visinger2/syn_result \
+python inference.py \
+    --model_dir "H:\repo\VISinger2\model" \
+    --input_dir "H:\repo\VISinger2\data\opencpop\testset.txt" \
+    --output_dir "H:\repo\VISinger2\data\opencpop\wavs"
 
